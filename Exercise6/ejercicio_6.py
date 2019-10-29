e3d6a8d8118945fc01a2d0dde5e8da89e28e555b
marianaiv <marianaalbrnz@gmail.com>
2019-10-29 01:12:16 -0400

    Finished spam classification and added ipynb file

--- Exercise6/ejercicio_6.py
+++ Exercise6/ejercicio_6.py
@@ -65,7 +65,7 @@ utils.plotData(X, y)
 # %%
 # You should try to change the C value below and see how the decision
 # boundary varies (e.g., try C = 1000)
-C = 1
+C = 1000
 
 model = utils.svmTrain(X, y, C, utils.linearKernel, 1e-3, 20)
 utils.visualizeBoundaryLinear(X, y, model)
@@ -100,7 +100,6 @@ def gaussianKernel(x1, x2, sigma):
     return sim
 # %% [markdown]
 # Once you have completed the function `gaussianKernel` the following cell will test your kernel function on two provided examples and you should expect to see a value of 0.324652.
-
 # %%
 x1 = np.array([1, 2, 1])
 x2 = np.array([0, 4, -1])
@@ -174,14 +173,14 @@ def dataset3Params(X, y, Xval, yval):
     testvalues = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
     best_error = 10000
 
-    for ii in testvalues:
-        for jj in testvalues:
-            predictions = utils.svmPredict(utils.svmTrain(X, y, ii, gaussianKernel, args=(jj,)), Xval)
+    for C_i in testvalues:
+        for sigma_j in testvalues:
+            predictions = utils.svmPredict(utils.svmTrain(X, y, C_i, gaussianKernel, args=(sigma_j,)), Xval)
             error = np.mean(predictions != yval)
             if error < best_error:
                 best_error = error
-                C = ii
-                sigma = jj
+                C = C_i
+                sigma = sigma_j
     
     # ============================================================
     return C, sigma
@@ -195,3 +194,308 @@ C, sigma = dataset3Params(X, y, Xval, yval)
 model = utils.svmTrain(X, y, C, gaussianKernel, args=(sigma,))
 utils.visualizeBoundary(X, y, model)
 print(C, sigma)
+# %% [markdown]
+# <a id="section3"></a>
+# ## 2 Spam Classification
+# 
+# Many email services today provide spam filters that are able to classify emails into spam and non-spam email with high accuracy. In this part of the exercise, you will use SVMs to build your own spam filter.
+# 
+# You will be training a classifier to classify whether a given email, $x$, is spam ($y = 1$) or non-spam ($y = 0$). In particular, you need to convert each email into a feature vector $x \in \mathbb{R}^n$ . The following parts of the exercise will walk you through how such a feature vector can be constructed from an email.
+# 
+# The dataset included for this exercise is based on a a subset of the [SpamAssassin Public Corpus](http://spamassassin.apache.org/old/publiccorpus/). For the purpose of this exercise, you will only be using the body of the email (excluding the email headers).
+# %% [markdown]
+# ### 2.1 Preprocessing Emails
+# 
+# Before starting on a machine learning task, it is usually insightful to take a look at examples from the dataset. The figure below shows a sample email that contains a URL, an email address (at the end), numbers, and dollar
+# amounts.
+# 
+# <img src="Figures/email.png" width="700px" />
+# 
+# While many emails would contain similar types of entities (e.g., numbers, other URLs, or other email addresses), the specific entities (e.g., the specific URL or specific dollar amount) will be different in almost every
+# email. Therefore, one method often employed in processing emails is to “normalize” these values, so that all URLs are treated the same, all numbers are treated the same, etc. For example, we could replace each URL in the
+# email with the unique string “httpaddr” to indicate that a URL was present.
+# 
+# This has the effect of letting the spam classifier make a classification decision based on whether any URL was present, rather than whether a specific URL was present. This typically improves the performance of a spam classifier, since spammers often randomize the URLs, and thus the odds of seeing any particular URL again in a new piece of spam is very small. 
+# 
+# In the function `processEmail` below, we have implemented the following email preprocessing and normalization steps:
+# 
+# - **Lower-casing**: The entire email is converted into lower case, so that captialization is ignored (e.g., IndIcaTE is treated the same as Indicate).
+# 
+# - **Stripping HTML**: All HTML tags are removed from the emails. Many emails often come with HTML formatting; we remove all the HTML tags, so that only the content remains.
+# 
+# - **Normalizing URLs**: All URLs are replaced with the text “httpaddr”.
+# 
+# - **Normalizing Email Addresses**:  All email addresses are replaced with the text “emailaddr”.
+# 
+# - **Normalizing Numbers**: All numbers are replaced with the text “number”.
+# 
+# - **Normalizing Dollars**: All dollar signs ($) are replaced with the text “dollar”.
+# 
+# - **Word Stemming**: Words are reduced to their stemmed form. For example, “discount”, “discounts”, “discounted” and “discounting” are all replaced with “discount”. Sometimes, the Stemmer actually strips off additional characters from the end, so “include”, “includes”, “included”, and “including” are all replaced with “includ”.
+# 
+# - **Removal of non-words**: Non-words and punctuation have been removed. All white spaces (tabs, newlines, spaces) have all been trimmed to a single space character.
+# 
+# The result of these preprocessing steps is shown in the figure below. 
+# 
+# <img src="Figures/email_cleaned.png" alt="email cleaned" style="width: 600px;"/>
+# 
+# While preprocessing has left word fragments and non-words, this form turns out to be much easier to work with for performing feature extraction.
+# %% [markdown]
+# #### 2.1.1 Vocabulary List
+# 
+# After preprocessing the emails, we have a list of words for each email. The next step is to choose which words we would like to use in our classifier and which we would want to leave out.
+# 
+# For this exercise, we have chosen only the most frequently occuring words as our set of words considered (the vocabulary list). Since words that occur rarely in the training set are only in a few emails, they might cause the
+# model to overfit our training set. The complete vocabulary list is in the file `vocab.txt` (inside the `Data` directory for this exercise) and also shown in the figure below.
+# 
+# <img src="Figures/vocab.png" alt="Vocab" width="150px" />
+# 
+# Our vocabulary list was selected by choosing all words which occur at least a 100 times in the spam corpus,
+# resulting in a list of 1899 words. In practice, a vocabulary list with about 10,000 to 50,000 words is often used.
+# Given the vocabulary list, we can now map each word in the preprocessed emails into a list of word indices that contains the index of the word in the vocabulary dictionary. The figure below shows the mapping for the sample email. Specifically, in the sample email, the word “anyone” was first normalized to “anyon” and then mapped onto the index 86 in the vocabulary list.
+# 
+# <img src="Figures/word_indices.png" alt="word indices" width="200px" />
+# 
+# Your task now is to complete the code in the function `processEmail` to perform this mapping. In the code, you are given a string `word` which is a single word from the processed email. You should look up the word in the vocabulary list `vocabList`. If the word exists in the list, you should add the index of the word into the `word_indices` variable. If the word does not exist, and is therefore not in the vocabulary, you can skip the word.
+#
+# <a id="processEmail"></a>
+# %%
+def processEmail(email_contents, verbose=True):
+    """
+    Instructions
+    ------------
+    Fill in this function to add the index of word to word_indices 
+    if it is in the vocabulary. At this point of the code, you have 
+    a stemmed word from the email in the variable word.
+    You should look up word in the vocabulary list (vocabList). 
+    If a match exists, you should add the index of the word to the word_indices
+    list.
+    
+    Notes
+    -----
+    - vocabList[idx] returns a the word with index idx in the vocabulary list.
+    
+    - vocabList.index(word) return index of word `word` in the vocabulary list.
+      (A ValueError exception is raised if the word does not exist.)
+    """
+    # Load Vocabulary
+    vocabList = utils.getVocabList()
+
+    # Init return value
+    word_indices = []
+
+    # ========================== Preprocess Email ===========================
+    # Find the Headers ( \n\n and remove )
+    # Uncomment the following lines if you are working with raw emails with the
+    # full headers
+    # hdrstart = email_contents.find(chr(10) + chr(10))
+    # email_contents = email_contents[hdrstart:]
+
+    # Lower case
+    email_contents = email_contents.lower()
+    
+    # Strip all HTML
+    # Looks for any expression that starts with < and ends with > and replace
+    # and does not have any < or > in the tag it with a space
+    email_contents =re.compile('<[^<>]+>').sub(' ', email_contents)
+
+    # Handle Numbers
+    # Look for one or more characters between 0-9
+    email_contents = re.compile('[0-9]+').sub(' number ', email_contents)
+
+    # Handle URLS
+    # Look for strings starting with http:// or https://
+    email_contents = re.compile('(http|https)://[^\s]*').sub(' httpaddr ', email_contents)
+
+    # Handle Email Addresses
+    # Look for strings with @ in the middle
+    email_contents = re.compile('[^\s]+@[^\s]+').sub(' emailaddr ', email_contents)
+    
+    # Handle $ sign
+    email_contents = re.compile('[$]+').sub(' dollar ', email_contents)
+    
+    # get rid of any punctuation
+    email_contents = re.split('[ @$/#.-:&*+=\[\]?!(){},''">_<;%\n\r]', email_contents)
+
+    # remove any empty word string
+    email_contents = [word for word in email_contents if len(word) > 0]
+    
+    # Stem the email contents word by word
+    stemmer = utils.PorterStemmer()
+    processed_email = []
+    for word in email_contents:
+        # Remove any remaining non alphanumeric characters in word
+        word = re.compile('[^a-zA-Z0-9]').sub('', word).strip()
+        word = stemmer.stem(word)
+        processed_email.append(word)
+
+        if len(word) < 1:
+            continue
+
+        # Look up the word in the dictionary and add to word_indices if found
+        # ====================== YOUR CODE HERE ======================
+        try:
+            word_indices.append(vocabList.index(word))
+        except ValueError:
+            pass
+        
+        # =============================================================
+
+    if verbose:
+        print('----------------')
+        print('Processed email:')
+        print('----------------')
+        print(' '.join(processed_email))
+    return word_indices
+# %% [markdown]
+# Once you have implemented `processEmail`, the following cell will run your code on the email sample and you should see an output of the processed email and the indices list mapping.
+# %%
+#  To use an SVM to classify emails into Spam v.s. Non-Spam, you first need
+#  to convert each email into a vector of features. In this part, you will
+#  implement the preprocessing steps for each email. You should
+#  complete the code in processEmail.m to produce a word indices vector
+#  for a given email.
+
+# Extract Features
+with open(os.path.join('Data', 'emailSample1.txt')) as fid:
+    file_contents = fid.read()
+
+word_indices  = processEmail(file_contents)
+
+#Print Stats
+print('-------------')
+print('Word Indices:')
+print('-------------')
+print(word_indices)
+# %% [markdown]
+# <a id="section4"></a>
+# ### 2.2 Extracting Features from Emails
+# 
+# You will now implement the feature extraction that converts each email into a vector in $\mathbb{R}^n$. For this exercise, you will be using n = # words in vocabulary list. Specifically, the feature $x_i \in \{0, 1\}$ for an email corresponds to whether the $i^{th}$ word in the dictionary occurs in the email. That is, $x_i = 1$ if the $i^{th}$ word is in the email and $x_i = 0$ if the $i^{th}$ word is not present in the email.
+# 
+# Thus, for a typical email, this feature would look like:
+# 
+# $$ x = \begin{bmatrix} 
+# 0 & \dots & 1 & 0 & \dots & 1 & 0 & \dots & 0 
+# \end{bmatrix}^T \in \mathbb{R}^n
+# $$
+# 
+# You should now complete the code in the function `emailFeatures` to generate a feature vector for an email, given the `word_indices`.
+# <a id="emailFeatures"></a>
+# %%
+def emailFeatures(word_indices):
+    """  
+    Instructions
+    ------------
+    Fill in this function to return a feature vector for the
+    given email (word_indices). To help make it easier to  process 
+    the emails, we have have already pre-processed each email and converted
+    each word in the email into an index in a fixed dictionary (of 1899 words).
+    The variable `word_indices` contains the list of indices of the words 
+    which occur in one email.
+
+    """
+    # Total number of words in the dictionary
+    n = 1899
+
+    # You need to return the following variables correctly.
+    x = np.zeros(n)
+
+    # ===================== YOUR CODE HERE ======================
+
+    for index in word_indices:
+        x[index] = 1
+    
+    # ===========================================================
+    
+    return x
+# %% [markdown]
+# Once you have implemented `emailFeatures`, the next cell will run your code on the email sample. You should see that the feature vector had length 1899 and 45 non-zero entries.
+# %%
+# Extract Features
+with open(os.path.join('Data', 'emailSample1.txt')) as fid:
+    file_contents = fid.read()
+
+word_indices  = processEmail(file_contents)
+features      = emailFeatures(word_indices)
+
+# Print Stats
+print('\nLength of feature vector: %d' % len(features))
+print('Number of non-zero entries: %d' % sum(features > 0))
+# %% [markdown]
+# ### 2.3 Training SVM for Spam Classification
+# 
+# In the following section we will load a preprocessed training dataset that will be used to train a SVM classifier. The file `spamTrain.mat` (within the `Data` folder for this exercise) contains 4000 training examples of spam and non-spam email, while `spamTest.mat` contains 1000 test examples. Each
+# original email was processed using the `processEmail` and `emailFeatures` functions and converted into a vector $x^{(i)} \in \mathbb{R}^{1899}$.
+# 
+# After loading the dataset, the next cell proceed to train a linear SVM to classify between spam ($y = 1$) and non-spam ($y = 0$) emails. Once the training completes, you should see that the classifier gets a training accuracy of about 99.8% and a test accuracy of about 98.5%.
+# %%
+# Load the Spam Email dataset
+# You will have X, y in your environment
+data = loadmat(os.path.join('Data', 'spamTrain.mat'))
+X, y= data['X'].astype(float), data['y'][:, 0]
+
+print('Training Linear SVM (Spam Classification)')
+print('This may take 1 to 2 minutes ...\n')
+
+C = 0.1
+model = utils.svmTrain(X, y, C, utils.linearKernel)
+# %%
+# Compute the training accuracy
+p = utils.svmPredict(model, X)
+
+print('Training Accuracy: %.2f' % (np.mean(p == y) * 100))
+# %% [markdown]
+# Execute the following cell to load the test set and compute the test accuracy.
+# %%
+# Load the test dataset
+# You will have Xtest, ytest in your environment
+data = loadmat(os.path.join('Data', 'spamTest.mat'))
+Xtest, ytest = data['Xtest'].astype(float), data['ytest'][:, 0]
+
+print('Evaluating the trained Linear SVM on a test set ...')
+p = utils.svmPredict(model, Xtest)
+
+print('Test Accuracy: %.2f' % (np.mean(p == ytest) * 100))
+# %% [markdown]
+# ### 2.4 Top Predictors for Spam
+# 
+# To better understand how the spam classifier works, we can inspect the parameters to see which words the classifier thinks are the most predictive of spam. The next cell finds the parameters with the largest positive values in the classifier and displays the corresponding words similar to the ones shown in the figure below.
+# 
+# <div style="border-style: solid; border-width: 1px; margin: 10px 10px 10px 10px; padding: 10px 10px 10px 10px">
+# our  click  remov guarante visit basenumb dollar pleas price will nbsp most lo ga hour
+# </div>
+# 
+# Thus, if an email contains words such as “guarantee”, “remove”, “dollar”, and “price” (the top predictors shown in the figure), it is likely to be classified as spam.
+# 
+# Since the model we are training is a linear SVM, we can inspect the weights learned by the model to understand better how it is determining whether an email is spam or not. The following code finds the words with the highest weights in the classifier. Informally, the classifier 'thinks' that these words are the most likely indicators of spam.
+# %%
+# Sort the weights and obtin the vocabulary list
+# NOTE some words have the same weights, 
+# so their order might be different than in the text above
+idx = np.argsort(model['w'])
+top_idx = idx[-15:][::-1]
+vocabList = utils.getVocabList()
+
+print('Top predictors of spam:')
+print('%-15s %-15s' % ('word', 'weight'))
+print('----' + ' '*12 + '------')
+for word, w in zip(np.array(vocabList)[top_idx], model['w'][top_idx]):
+    print('%-15s %0.2f' % (word, w))
+# %% [markdown]
+# ### 2.5 Optional (ungraded) exercise: Try your own emails
+# 
+# Now that you have trained a spam classifier, you can start trying it out on your own emails. In the starter code, we have included two email examples (`emailSample1.txt` and `emailSample2.txt`) and two spam examples (`spamSample1.txt` and `spamSample2.txt`). The next cell runs the spam classifier over the first spam example and classifies it using the learned SVM. You should now try the other examples we have provided and see if the classifier gets them right. You can also try your own emails by replacing the examples (plain text files) with your own emails.
+# %%
+filename = os.path.join('Data', 'spamSample1.txt')
+
+with open(filename) as fid:
+    file_contents = fid.read()
+
+word_indices = processEmail(file_contents, verbose=False)
+x = emailFeatures(word_indices)
+p = utils.svmPredict(model, x)
+
+print('\nProcessed %s\nSpam Classification: %s' % (filename, 'spam' if p else 'not spam'))
+
+# %%
