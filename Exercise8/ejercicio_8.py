fea50608903120044e58fd204a7e4ea574d4cff1
marianaiv <marianaalbrnz@gmail.com>
2019-11-02 19:00:31 -0400

    Recommender systems section done and ipynb file

--- Exercise8/ejercicio_8.py
+++ Exercise8/ejercicio_8.py
@@ -266,3 +266,309 @@ print('  (you should see a value epsilon of about 1.38e-18)')
 print('   (you should see a Best F1 value of      0.615385)')
 print('\n# Outliers found: %d' % np.sum(p < epsilon))
 # %%
+# %% [markdown]
+# ## 2 Recommender Systems
+# 
+# In this part of the exercise, you will implement the collaborative filtering learning algorithm and apply it to a dataset of movie ratings ([MovieLens 100k Dataset](https://grouplens.org/datasets/movielens/) from GroupLens Research). This dataset consists of ratings on a scale of 1 to 5. The dataset has $n_u = 943$ users, and $n_m = 1682$ movies. 
+# 
+# In the next parts of this exercise, you will implement the function `cofiCostFunc` that computes the collaborative filtering objective function and gradient. After implementing the cost function and gradient, you will use `scipy.optimize.minimize` to learn the parameters for collaborative filtering.
+# 
+# ### 2.1 Movie ratings dataset
+# 
+# The next cell will load the dataset `ex8_movies.mat`, providing the variables `Y` and `R`.
+# The matrix `Y` (a `num_movies` $\times$ `num_users` matrix) stores the ratings $y^{(i,j)}$ (from 1 to 5). The matrix `R` is an binary-valued indicator matrix, where $R(i, j) = 1$ if user $j$ gave a rating to movie $i$, and $R(i, j) = 0$ otherwise. The objective of collaborative filtering is to predict movie ratings for the movies that users have not yet rated, that is, the entries with $R(i, j) = 0$. This will allow us to recommend the movies with the highest predicted ratings to the user.
+# 
+# To help you understand the matrix `Y`, the following cell will compute the average movie rating for the first movie (Toy Story) and print its average rating.
+# %%
+# Load data
+data = loadmat(os.path.join('Data', 'ex8_movies.mat'))
+Y, R = data['Y'], data['R']
+
+# Y is a 1682x943 matrix, containing ratings (1-5) of 
+# 1682 movies on 943 users
+
+# R is a 1682x943 matrix, where R(i,j) = 1 
+# if and only if user j gave a rating to movie i
+
+# From the matrix, we can compute statistics like average rating.
+print('Average rating for movie 1 (Toy Story): %f / 5' %
+      np.mean(Y[0, R[0, :] == 1]))
+
+# We can "visualize" the ratings matrix by plotting it with imshow
+pyplot.figure(figsize=(8, 8))
+pyplot.imshow(Y)
+pyplot.ylabel('Movies')
+pyplot.xlabel('Users')
+pyplot.grid(False)
+# %% [markdown]
+# Throughout this part of the exercise, you will also be working with the matrices, `X` and `Theta`:
+# 
+# $$ \text{X} = 
+# \begin{bmatrix}
+# - \left(x^{(1)}\right)^T - \\
+# - \left(x^{(2)}\right)^T - \\
+# \vdots \\
+# - \left(x^{(n_m)}\right)^T - \\
+# \end{bmatrix}, \quad
+# \text{Theta} = 
+# \begin{bmatrix}
+# - \left(\theta^{(1)}\right)^T - \\
+# - \left(\theta^{(2)}\right)^T - \\
+# \vdots \\
+# - \left(\theta^{(n_u)}\right)^T - \\
+# \end{bmatrix}.
+# $$
+# 
+# The $i^{th}$ row of `X` corresponds to the feature vector $x^{(i)}$ for the $i^{th}$ movie, and the $j^{th}$ row of `Theta` corresponds to one parameter vector $\theta^{(j)}$, for the $j^{th}$ user. Both $x^{(i)}$ and $\theta^{(j)}$ are n-dimensional vectors. For the purposes of this exercise, you will use $n = 100$, and therefore, $x^{(i)} \in \mathbb{R}^{100}$ and $\theta^{(j)} \in \mathbb{R}^{100}$. Correspondingly, `X` is a $n_m \times 100$ matrix and `Theta` is a $n_u \times 100$ matrix.
+# 
+# <a id="section3"></a>
+# ### 2.2 Collaborative filtering learning algorithm
+# 
+# Now, you will start implementing the collaborative filtering learning algorithm. You will start by implementing the cost function (without regularization).
+# 
+# The collaborative filtering algorithm in the setting of movie recommendations considers a set of n-dimensional parameter vectors $x^{(1)}, \dots, x^{(n_m)}$ and $\theta^{(1)} , \dots, \theta^{(n_u)}$, where the model predicts the rating for movie $i$ by user $j$ as $y^{(i,j)} = \left( \theta^{(j)} \right)^T x^{(i)}$. Given a dataset that consists of a set of ratings produced by some users on some movies, you wish to learn the parameter vectors $x^{(1)}, \dots, x^{(n_m)}, \theta^{(1)}, \dots, \theta^{(n_u)}$ that produce the best fit (minimizes the squared error).
+# 
+# You will complete the code in `cofiCostFunc` to compute the cost function and gradient for collaborative filtering. Note that the parameters to the function (i.e., the values that you are trying to learn) are `X` and `Theta`. In order to use an off-the-shelf minimizer such as `scipy`'s `minimize` function, the cost function has been set up to unroll the parameters into a single vector called `params`. You had previously used the same vector unrolling method in the neural networks programming exercise.
+# 
+# #### 2.2.1 Collaborative filtering cost function
+# 
+# The collaborative filtering cost function (without regularization) is given by
+# 
+# $$
+# J(x^{(1)}, \dots, x^{(n_m)}, \theta^{(1)}, \dots,\theta^{(n_u)}) = \frac{1}{2} \sum_{(i,j):r(i,j)=1} \left( \left(\theta^{(j)}\right)^T x^{(i)} - y^{(i,j)} \right)^2
+# $$
+# 
+# You should now modify the function `cofiCostFunc` to return this cost in the variable `J`. Note that you should be accumulating the cost for user $j$ and movie $i$ only if `R[i,j] = 1`.
+# %%
+def cofiCostFunc(params, Y, R, num_users, num_movies,
+                      num_features, lambda_=0.0):
+    """
+    Instructions
+    ------------
+    Compute the cost function and gradient for collaborative filtering.
+    Concretely, you should first implement the cost function (without
+    regularization) and make sure it is matches our costs. After that,
+    you should implement thegradient and use the checkCostFunction routine 
+    to check that the gradient is correct. Finally, you should implement
+    regularization.
+    
+    """
+    # Unfold the U and W matrices from params
+    X = params[:num_movies*num_features].reshape(num_movies, num_features)
+    Theta = params[num_movies*num_features:].reshape(num_users, num_features)
+
+    # You need to return the following values correctly
+    J = 0
+    X_grad = np.zeros(X.shape)
+    Theta_grad = np.zeros(Theta.shape)
+
+    # ====================== YOUR CODE HERE ======================
+
+    # Common term
+    diff = (np.dot(X, Theta.T)-Y)*R
+
+    # Computing cost and gradient
+    J = (1/2)*np.sum(np.square(diff))
+    X_grad = np.dot(diff, Theta)
+    Theta_grad = np.dot(diff.T, X)
+
+    # Regularization
+    J += (lambda_/2)*(np.sum(Theta*Theta)+np.sum(X*X))
+    X_grad += lambda_*X
+    Theta_grad += lambda_*Theta
+    
+    # =============================================================
+    
+    grad = np.concatenate([X_grad.ravel(), Theta_grad.ravel()])
+    return J, grad
+# %% [markdown]
+# After you have completed the function, the next cell will run your cost function. To help you debug your cost function, we have included set of weights that we trained on that.  You should expect to see an output of 22.22.
+# %%
+#  Load pre-trained weights (X, Theta, num_users, num_movies, num_features)
+data = loadmat(os.path.join('Data', 'ex8_movieParams.mat'))
+X, Theta, num_users, num_movies, num_features = data['X'],        data['Theta'], data['num_users'], data['num_movies'], data['num_features']
+
+#  Reduce the data set size so that this runs faster
+num_users = 4
+num_movies = 5
+num_features = 3
+
+X = X[:num_movies, :num_features]
+Theta = Theta[:num_users, :num_features]
+Y = Y[:num_movies, 0:num_users]
+R = R[:num_movies, 0:num_users]
+
+#  Evaluate cost function
+J, _ = cofiCostFunc(np.concatenate([X.ravel(), Theta.ravel()]),
+                    Y, R, num_users, num_movies, num_features)
+           
+print('Cost at loaded parameters:  %.2f \n(this value should be about 22.22)' % J)
+# %% [markdown]
+# <a id="section4"></a>
+# #### 2.2.2 Collaborative filtering gradient
+# 
+# Now you should implement the gradient (without regularization). Specifically, you should complete the code in `cofiCostFunc` to return the variables `X_grad` and `Theta_grad`. Note that `X_grad` should be a matrix of the same size as `X` and similarly, `Theta_grad` is a matrix of the same size as
+# `Theta`. The gradients of the cost function is given by:
+# 
+# $$ \frac{\partial J}{\partial x_k^{(i)}} = \sum_{j:r(i,j)=1} \left( \left(\theta^{(j)}\right)^T x^{(i)} - y^{(i,j)} \right) \theta_k^{(j)} $$
+# 
+# $$ \frac{\partial J}{\partial \theta_k^{(j)}} = \sum_{i:r(i,j)=1} \left( \left(\theta^{(j)}\right)^T x^{(i)}- y^{(i,j)} \right) x_k^{(j)} $$
+# 
+# Note that the function returns the gradient for both sets of variables by unrolling them into a single vector. After you have completed the code to compute the gradients, the next cell run a gradient check
+# (available in `utils.checkCostFunction`) to numerically check the implementation of your gradients (this is similar to the numerical check that you used in the neural networks exercise. If your implementation is correct, you should find that the analytical and numerical gradients match up closely.
+# %%
+#  Check gradients by running checkcostFunction
+utils.checkCostFunction(cofiCostFunc)
+# %% [markdown]
+# <a id="section5"></a>
+# #### 2.2.3 Regularized cost function
+# 
+# The cost function for collaborative filtering with regularization is given by
+# 
+# $$ J(x^{(1)}, \dots, x^{(n_m)}, \theta^{(1)}, \dots, \theta^{(n_u)}) = \frac{1}{2} \sum_{(i,j):r(i,j)=1} \left( \left( \theta^{(j)} \right)^T x^{(i)} - y^{(i,j)} \right)^2 + \left( \frac{\lambda}{2} \sum_{j=1}^{n_u} \sum_{k=1}^{n} \left( \theta_k^{(j)} \right)^2  \right) + \left( \frac{\lambda}{2} \sum_{i=1}^{n_m} \sum_{k=1}^n \left(x_k^{(i)} \right)^2 \right) $$
+# 
+# You should now add regularization to your original computations of the cost function, $J$. After you are done, the next cell will run your regularized cost function, and you should expect to see a cost of about 31.34.
+# %%
+#  Evaluate cost function
+J, _ = cofiCostFunc(np.concatenate([X.ravel(), Theta.ravel()]),
+                    Y, R, num_users, num_movies, num_features, 1.5)
+           
+print('Cost at loaded parameters (lambda = 1.5): %.2f' % J)
+print('              (this value should be about 31.34)')
+# %% [markdown]
+# <a id="section6"></a>
+# #### 2.2.4 Regularized gradient
+# 
+# Now that you have implemented the regularized cost function, you should proceed to implement regularization for the gradient. You should add to your implementation in `cofiCostFunc` to return the regularized gradient
+# by adding the contributions from the regularization terms. Note that the gradients for the regularized cost function is given by:
+# 
+# $$ \frac{\partial J}{\partial x_k^{(i)}} = \sum_{j:r(i,j)=1} \left( \left(\theta^{(j)}\right)^T x^{(i)} - y^{(i,j)} \right) \theta_k^{(j)} + \lambda x_k^{(i)} $$
+# 
+# $$ \frac{\partial J}{\partial \theta_k^{(j)}} = \sum_{i:r(i,j)=1} \left( \left(\theta^{(j)}\right)^T x^{(i)}- y^{(i,j)} \right) x_k^{(j)} + \lambda \theta_k^{(j)} $$
+# 
+# This means that you just need to add $\lambda x^{(i)}$ to the `X_grad[i,:]` variable described earlier, and add $\lambda \theta^{(j)}$ to the `Theta_grad[j, :]` variable described earlier.
+# 
+# After you have completed the code to compute the gradients, the following cell will run another gradient check (`utils.checkCostFunction`) to numerically check the implementation of your gradients.
+# %%
+#  Check gradients by running checkCostFunction
+utils.checkCostFunction(cofiCostFunc, 1.5)
+# %% [markdown]
+# ### 2.3 Learning movie recommendations 
+# 
+# After you have finished implementing the collaborative filtering cost function and gradient, you can now start training your algorithm to make movie recommendations for yourself. In the next cell, you can enter your own movie preferences, so that later when the algorithm runs, you can get your own movie recommendations! We have filled out some values according to our own preferences, but you should change this according to your own tastes. The list of all movies and their number in the dataset can be found listed in the file `Data/movie_idx.txt`.
+# %%
+#  Before we will train the collaborative filtering model, we will first
+#  add ratings that correspond to a new user that we just observed. This
+#  part of the code will also allow you to put in your own ratings for the
+#  movies in our dataset!
+movieList = utils.loadMovieList()
+n_m = len(movieList)
+
+#  Initialize my ratings
+my_ratings = np.zeros(n_m)
+
+# Check the file movie_idx.txt for id of each movie in our dataset
+# For example, Toy Story (1995) has ID 1, so to rate it "4", you can set
+# Note that the index here is ID-1, since we start index from 0.
+my_ratings[0] = 4
+
+# Or suppose did not enjoy Silence of the Lambs (1991), you can set
+my_ratings[97] = 2
+
+# We have selected a few movies we liked / did not like and the ratings we
+# gave are as follows:
+my_ratings[6] = 3
+my_ratings[11]= 5
+my_ratings[53] = 4
+my_ratings[63] = 5
+my_ratings[65] = 3
+my_ratings[68] = 5
+my_ratings[182] = 4
+my_ratings[225] = 5
+my_ratings[354] = 5
+
+print('New user ratings:')
+print('-----------------')
+for i in range(len(my_ratings)):
+    if my_ratings[i] > 0:
+        print('Rated %d stars: %s' % (my_ratings[i], movieList[i]))
+# %% [markdown]
+# #### 2.3.1 Recommendations
+# 
+# After the additional ratings have been added to the dataset, the script
+# will proceed to train the collaborative filtering model. This will learn the
+# parameters X and Theta. To predict the rating of movie i for user j, you need to compute (θ (j) ) T x (i) . The next part of the script computes the ratings for
+# all the movies and users and displays the movies that it recommends (Figure
+# 4), according to ratings that were entered earlier in the script. Note that
+# you might obtain a different set of the predictions due to different random
+# initializations.
+# %%
+#  Now, you will train the collaborative filtering model on a movie rating 
+#  dataset of 1682 movies and 943 users
+
+#  Load data
+data = loadmat(os.path.join('Data', 'ex8_movies.mat'))
+Y, R = data['Y'], data['R']
+
+#  Y is a 1682x943 matrix, containing ratings (1-5) of 1682 movies by 
+#  943 users
+
+#  R is a 1682x943 matrix, where R(i,j) = 1 if and only if user j gave a
+#  rating to movie i
+
+#  Add our own ratings to the data matrix
+Y = np.hstack([my_ratings[:, None], Y])
+R = np.hstack([(my_ratings > 0)[:, None], R])
+
+#  Normalize Ratings
+Ynorm, Ymean = utils.normalizeRatings(Y, R)
+
+#  Useful Values
+num_movies, num_users = Y.shape
+num_features = 10
+
+# Set Initial Parameters (Theta, X)
+X = np.random.randn(num_movies, num_features)
+Theta = np.random.randn(num_users, num_features)
+
+initial_parameters = np.concatenate([X.ravel(), Theta.ravel()])
+
+# Set options for scipy.optimize.minimize
+options = {'maxiter': 100}
+
+# Set Regularization
+lambda_ = 10
+res = optimize.minimize(lambda x: cofiCostFunc(x, Ynorm, R, num_users,
+                                               num_movies, num_features, lambda_),
+                        initial_parameters,
+                        method='TNC',
+                        jac=True,
+                        options=options)
+theta = res.x
+
+# Unfold the returned theta back into U and W
+X = theta[:num_movies*num_features].reshape(num_movies, num_features)
+Theta = theta[num_movies*num_features:].reshape(num_users, num_features)
+
+print('Recommender system learning completed.')
+# %% [markdown]
+# After training the model, you can now make recommendations by computing the predictions matrix.
+# %%
+p = np.dot(X, Theta.T)
+my_predictions = p[:, 0] + Ymean
+
+movieList = utils.loadMovieList()
+
+ix = np.argsort(my_predictions)[::-1]
+
+print('Top recommendations for you:')
+print('----------------------------')
+for i in range(10):
+    j = ix[i]
+    print('Predicting rating %.1f for movie %s' % (my_predictions[j], movieList[j]))
+
+print('\nOriginal ratings provided:')
+print('--------------------------')
+for i in range(len(my_ratings)):
+    if my_ratings[i] > 0:
+        print('Rated %d for %s' % (my_ratings[i], movieList[i]))
